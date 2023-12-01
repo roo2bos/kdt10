df17eb70345c3e752c148891c54eeb5d8e8657ee
김상호(KDT web 10) <roo2bos@gmail.com>
2023-12-01 10:06:41 +0900

    수정 : Cmain의 db의 유저 데이터 반환으로 변경

--- 14-mvc-practice/controller/Cmain.js
+++ 14-mvc-practice/controller/Cmain.js
@@ -7,7 +7,8 @@ exports.main = (req,res)=>{
 exports.login = (req, res) => {
     const { id , pw } = req.body;
     if (Database.userInfo.id == id && Database.userInfo.pw == pw) {
-        res.send({userInfo: req.body, isSuccess:true});
+        // res.send({userInfo: req.body, isSuccess:true});
+        res.send({userInfo: Database.userInfo, isSuccess:true});// 실제 db의 유저 데이터(Database.userInfo)를 반환해 줌
     }else{
         res.send({isSuccess:false});
     }
