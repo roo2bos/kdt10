4bef1c23dea39558995b065b7a6a2398cbb32914
김상호(KDT web 10) <roo2bos@gmail.com>
2023-12-01 09:52:34 +0900

    기존 mvc-prac 폴더 삭제

--- 14-mvc-prac/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../controller/Cmain');
-router.get('/',controller.main);
-router.post('/login',controller.login);
-
-module.exports = router;
